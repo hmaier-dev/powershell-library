771d44abb6726fc620f4b1e47a9b64086b047023
Hendrik Maier <hmaier@ipb-halle.de>
2023-09-20 15:37:51 +0200

    update script for dell docking station

---
+++ src/dell-docks-driver-update.ps1
@@ -0,0 +1,7 @@
+
+Install-Module -Name DellBIOSProvider -Force -Scope AllUsers
+Import-Module DellBIOSProvider
+
+## Get-DellDriverPackage -DeviceType Dock
+## Invoke-DellDriverUpdate -Download -DeviceType Dock
+## Invoke-DellDriverUpdate -Install -DeviceType Dock
